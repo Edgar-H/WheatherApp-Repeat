6dc87949bd458ee625dfc44d8856223296fda1ab
Edgar Hernandez <edgar.hernandezvh@gmail.com>
2022-01-03 18:52:08 -0600

    Update getGeoIp.js

--- src/assets/getGeoIp.js
+++ src/assets/getGeoIp.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getGeoIp = async () => {
   const { data } = await axios
-    .get(`http://ip-api.com/json`)
+    .get(`https://ip-api.com/json`)
     .then((res) => res)
     .catch((err) => console.log(err));
   return data;
